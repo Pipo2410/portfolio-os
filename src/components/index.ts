37a0ed1d64e537301de22b770326c6a38a94ae9b
Pipo2410 <37326909+Pipo2410@users.noreply.github.com>
2025-12-01 21:37:59 +0000

    11: Add projects to desktop

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import { Welcome } from './Welcome'
 import { Navbar } from './Navbar'
 import { Dock } from './Dock'
 import { WindowControls } from './WindowControls'
+import { Home } from './Home'
 
-export { Welcome, Navbar, Dock, WindowControls }
+export { Welcome, Navbar, Dock, WindowControls, Home }
